f3d58e1c91bfbb5c4687d39ca1b7f6b4eacd4dac
Trinay Bhati <bhatiji780780@gmail.com>
2023-07-05 23:13:22 +0530

    5-july

--- 3-july/index.js
+++
@@ -1,4 +0,0 @@
-console.log("LFG");
-console.log("siiuuuuuuuuu");
-
- 
--- practice/index.js
+++ practice/index.js
@@ -1,5 +1,6 @@
 const os = require("node:os");
 const fs = require("node:fs");
+const fileName = "simple.txt";
 
 // console.log(os); // shows complete object
 
@@ -37,19 +38,69 @@ const fs = require("node:fs");
 
 // console.log(os.userInfo());
 
+// console.log(os.constants); // gives all numbers consts
+
 // fs.writeFile("sample.txt", "file created by fs module", (error)=>{
 //     console.log(error)
 // }) // to create file first then it'll update
 
-// fs.appendFile("sample.txt",  `${os.EOL} update it baby` ,(error)=>{
+// fs.appendFile("sample.txt",  `${os.EOL} update it ` ,(error)=>{
 //     console.log(error);
 // }) // to update, it'll continue in from the end
 
-fs.writeFile("Logs.txt", "creating Logs", (error)=>{
-    if(error){
-        console.log(error);
-    }
-});
+// fs.writeFile("Logs.txt", "creating Logs", (error)=>{
+//     if(error){
+//         console.log(error);
+//     }
+// });
 
-const msg = os.EOL+ new Date().toISOString() + " \n User Logged Out \n";
-fs.appendFile("Logs.txt" , msg , ()=>{})
+// const msg = os.EOL+ new Date().toISOString() + " \n User Logged Out \n";
+// fs.appendFile("Logs.txt" , msg , ()=>{})
+
+const updateFile = () => {
+  const msg = new Date().toISOString() + "User Login Activity\n"; //also can use toLocalString() instead of toISOString()
+  //Asynchronus way we prefer this
+  fs.appendFile("simple.txt", msg, (error) => {
+    if (error) {
+      console.log(error);
+    }
+  });
+};
+// updateFile();
+//synchronus way but we don't prefer it
+// fs.appendFile("Sample.txt", msg);
+
+const readFile = () => {
+  fs.readFile(fileName, (error, data) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    console.log("FILE_CONTENT", data.toString());
+  });
+};
+// readFile();
+
+const deleteFile = () => {
+  fs.unlink(fileName, (error) => {
+    if (error) {
+      console.log("error occured", error);
+      return;
+    }
+    console.log("FILE", fileName, "DELETED SUCCESSFULLY");
+  });
+};
+// deleteFile(); // deletes file
+
+const folderExists = () => {
+  const exists = fs.existsSync("logs");
+  console.log(exists);
+};
+// folderExists();
+
+const createFolder = () => {
+  fs.mkdir("logs", () => {
+    console.log("FOLDER CREATED SUCCESSFULLY");
+  });
+};
+// createFolder(); // creates folder -> mkdir()
