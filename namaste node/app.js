a66c600d6d533e70006079846d867c40d3679f51
TrinayBhati <bhatiji780780@gmail.com>
2024-11-27 18:14:00 +0530

    pushing class 9

--- namaste node/app.js
+++ namaste node/app.js
@@ -11,15 +11,39 @@ const log = console.log;
 //==========================================================================================
 // #4
 
-const { calculateSum, variable } = require("./xyz")
-const data = require("./data.json")
+// const { calculateSum, variable } = require("./xyz")
+// const data = require("./data.json")
 
-log("data : ", data)
+// log("data : ", data)
 // log("data : ", JSON.stringify(data))
 
 // import { calculateSum } from "./xyz.js"
 
-const ans = calculateSum(2, 3)
-log("variable : ", variable)
-log("answer : ", ans)
-log(__filename)
+// const ans = calculateSum(2, 3)
+// log("variable : ", variable)
+// log("answer : ", ans)
+// log(__filename)
+
+//===========================================================================================
+//#9
+
+const fs = require("fs");
+const a = 10;
+
+setImmediate(() => log("setimmideate print"));
+
+fs.readFile("./checkout-later.txt", "utf8", () => {
+    log("file reading done")
+});
+
+setTimeout(() => {
+    log("set timeout print")
+}, 0);
+
+function print() {
+log("print a : ", a)
+}
+
+print();
+
+log("end");
